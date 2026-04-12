1e49247a02c4dec46d73e1fa93a578930e03bc38
Rubel224603 <cserubelhosen@gmail.com>
2026-04-12 17:49:39 +0600

    ui added

--- include/header.php
+++ include/header.php
@@ -9,20 +9,3 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm sticky-top">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="#">BLOG_SYS</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navRes">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navRes">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Articles</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Categories</a></li>
-                    <li class="nav-item"><a class="btn btn-primary btn-sm ms-lg-3 text-white" href="login.php">Admin Login</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
---
+++ include/menu.php
@@ -0,0 +1,18 @@
+<?php include 'include/header.php'?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm sticky-top">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">BLOG_SYS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navRes">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navRes">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Articles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Categories</a></li>
+                    <li class="nav-item"><a class="btn btn-primary btn-sm ms-lg-3 text-white" href="login.php">Admin Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
