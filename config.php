f94d3f4979a5c725c15be07527bb0684bd9a756a
Rubel224603 <cserubelhosen@gmail.com>
2026-04-26 19:29:42 +0600

    add  magic constant dir ,file

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
     session_start();
-    // define("BASE_URL", "http://localhost/blog_manegement/"); //set base url
-    //define('BASE_URL', 'http://localhost/blog_manegement/');
-    define('BASE_URL', 'http://localhost/blog_manegement/');
-    define('ROOT', 'D:/xampp/htdocs/blog_manegement/');
+
+    // define('BASE_URL', 'http://localhost/blog_manegement/');
+    define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/' . basename(__DIR__) . '/');
+    //echo  $_SERVER['HTTP_HOST'];
+    
+    //define('ROOT', 'D:/xampp/htdocs/blog_manegement/');
+
+    define('ROOT', __DIR__ . '/'); //show current directory
+     //echo ROOT;
+     define("PATH",__FILE__); //show current file
+    // echo PATH;
 
     $host     = 'localhost';
     $db_name  = 'blog_manegement';
